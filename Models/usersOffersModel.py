98966bc2c30880aa292498cc8c5df8a940dade0e
Luis Miguel Aguirre Serna <miguiluis90@gmail.com>
2024-09-03 16:26:28 -0500

    modificado useroffer

--- Models/usersOffersModel.py
+++ Models/usersOffersModel.py
@@ -5,8 +5,8 @@ class UsersOffers(Base):
     __tablename__ = 'UsersOffers'
 
     user_offer_id = Column(Integer, primary_key=True, autoincrement=True)
-    role_id = Column(Integer, ForeignKey('Role.role_id'))  # ID del rol (llave foránea)
+    user_id = Column(Integer, ForeignKey('User.user_id'))  # ID del rol (llave foránea)
     offer_id = Column(Integer,ForeignKey('Offer.offer_id'))  # ID de la oferta (llave foránea)
 
     def __repr__(self):
-        return f"<UsersOffers(user_offer_id={self.user_offer_id}, role_id={self.role_id}, offer_id={self.offer_id})>"
+        return f"<UsersOffers(user_offer_id={self.user_offer_id}, user_id={self.user_id}, offer_id={self.offer_id})>"

98dc1aa0cca70726d2762ab673820e34638e6a94
WAYNE120804 <jhonsebastian-04@hotmail.com>
2024-10-12 18:42:39 -0500

    se agrego las clases userTaskType, userReview, supportTopicModel y la clase de taskModel se modifico para agregar los atributos task_name y task_address

--- Models/taskModel.py
+++ Models/taskModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey,DECIMAL
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey,DECIMAL, VARCHAR
 from Models.init import Base
 
 class Task(Base):
@@ -8,6 +8,8 @@ class Task(Base):
     task_offer_suggested = Column(DECIMAL)
     task_description = Column(Text(400))
     task_stimed_time_hours = Column(DECIMAL, nullable= False)
+    task_name = Column(VARCHAR(100), nullable= False)
+    task_address = Column(VARCHAR(100), nullable = False)
     task_type_id = Column(Integer, ForeignKey('TaskType.task_type_id'),nullable=False)
 
     def __repr__(self):
@@ -15,4 +17,6 @@ class Task(Base):
                 f"task_offer_suggested={self.task_offer_suggested}, "
                 f"task_description={self.task_description}, "
                 f"task_stimed_time_hours={self.task_stimed_time_hours}, "
+                f"task_name={self.task_name},"
+                f"task_address={self.task_name},"
                 f"task_type_id={self.task_type_id})>")
